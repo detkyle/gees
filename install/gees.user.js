e2fae1c70e74eefbc9468ffe97d68641210260ad
Jerry Batorski <jerry.batorski@gmail.com>
2018-07-01 17:53:55 -0500

    Update installer

--- install/gees.user.js
+++ install/gees.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name     GalacticEra Enhancement Suite
-// @version  0.4.0
+// @version  0.4.1
 // @homepage https://github.com/jarekb84/gees
 // @include  https://aall.space/*
 // @grant    GM_addStyle
