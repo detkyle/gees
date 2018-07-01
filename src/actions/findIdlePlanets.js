7f103e61b7916c0c87e15086195d491993d43f92
Jerry Batorski <jerry.batorski@gmail.com>
2018-07-01 15:17:39 -0500

    Rework idle task detection

--- src/actions/findIdlePlanets.js
+++ src/actions/findIdlePlanets.js
@@ -1,21 +1,20 @@
-/*global playerInfo, GetPlanetName */
+/*global playerInfo, GetPlanetName, gameTime */
 
 import React, { Component } from "react";
 import Button from "../common/button";
 import { populatePlanetData } from "../common/populatePlanetData";
 import _ from "lodash";
-
-const initialState = {
-  label: "0",
-  title: "No idle planets found\nClick to check again",
-  color: "green"
-};
+import moment from "moment";
 
 class FindIdlePlanets extends Component {
   constructor() {
     super();
-    this.state = initialState;
-    this.getIdlePlanets = this.getIdlePlanets.bind(this);
+    this.state = {
+      label: "0",
+      title: "",
+      color: "green"
+    };
+
     this.updateButtonLabel = this.updateButtonLabel.bind(this);
     this.startIdlePlanetDetection = this.startIdlePlanetDetection.bind(this);
   }
@@ -24,29 +23,82 @@ class FindIdlePlanets extends Component {
     this.startIdlePlanetDetection();
   }
 
-  getIdlePlanets() {
+  getPlanets() {
     const legion = playerInfo.playerArmy;
-    const allPlanets = legion.armyInfo.planets.map(planet => planet[0]);
-    const planetsWithRunningTasks = legion.services.getById
-      .filter(service => service.type !== "army_build")
-      .map(service => service.planet);
+    const allPlanets = legion.armyInfo.planets.reduce((result, item) => {
+      result[item[0]] = { name: GetPlanetName(item[0]) };
+      return result;
+    }, {});
 
-    const idlePlanets = _.difference(allPlanets, planetsWithRunningTasks);
-    return idlePlanets.map(planetId => GetPlanetName(planetId));
+    const planetsWithRunningTasks = legion.services.getById.filter(
+      service => service && service.type !== "army_build"
+    );
+
+    planetsWithRunningTasks.forEach(task => {
+      const secondsTillEnd = task.endTime - gameTime;
+      const ending = moment().add(secondsTillEnd, "seconds");
+
+      allPlanets[task.planet].ending = ending;
+      allPlanets[task.planet].day = ending.format("ddd");
+      allPlanets[task.planet].time = ending.format("HH:mm");
+    });
+
+    return allPlanets;
+  }
+
+  groupAndSort(planets) {
+    let idle = [];
+    let busy = [];
+    for (var id in planets) {
+      let planet = planets[id];
+      if (typeof planet.ending === "undefined") {
+        idle.push(planet.name);
+      } else {
+        busy.push(planet);
+      }
+    }
+
+    busy = _.sortBy(busy, ["ending"]);
+    busy = _.groupBy(busy, "day");
+
+    return {
+      idle,
+      busy
+    };
+  }
+
+  getPrintout(groups) {
+    let result = "";
+
+    for (var day in groups) {
+      result += `  ${day}\n`;
+
+      groups[day].forEach(task => {
+        result += `    ${task.time} ${task.name}\n`;
+      });
+    }
+
+    return result;
   }
 
   updateButtonLabel() {
-    const idlePlanets = this.getIdlePlanets();
+    const planets = this.getPlanets();
+    const { idle, busy } = this.groupAndSort(planets);
+    let color = "green";
+    let title = "";
 
-    if (idlePlanets.length > 0) {
-      this.setState({
-        label: idlePlanets.length,
-        color: "red",
-        title: `Idle Planets: \n${idlePlanets.join("\n")}`
-      });
-    } else {
-      this.setState(initialState);
+    if (idle.length > 0) {
+      color = "red";
+      title = `Idle Planets\n  ${idle.join("\n  ")}\n\n`;
     }
+
+    title += `Busy Planets until\n${this.getPrintout(busy)}`;
+
+    this.setState({
+      label: idle.length,
+      color,
+      title
+    });
   }
 
   startIdlePlanetDetection() {
